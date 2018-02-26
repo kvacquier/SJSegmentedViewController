a9dc05e9aaf0bb7c8c836a829ab51f4c254290c3
kvacquier <kvacquier@gmail.com>
2018-02-26 15:20:01 +0100

    qweq
    weqwe

---
+++ SJSegmentedScrollView/SJSegmentedScrollView.h
@@ -0,0 +1,19 @@
+//
+//  SJSegmentedScrollView.h
+//  SJSegmentedScrollView
+//
+//  Created by Kevin VACQUIER on 26/02/2018.
+//  Copyright © 2018 Kevin VACQUIER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SJSegmentedScrollView.
+FOUNDATION_EXPORT double SJSegmentedScrollViewVersionNumber;
+
+//! Project version string for SJSegmentedScrollView.
+FOUNDATION_EXPORT const unsigned char SJSegmentedScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SJSegmentedScrollView/PublicHeader.h>
+
+
